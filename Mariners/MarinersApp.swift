ea1cec8fb86a72b910e662a8158aa15d717a76d4
Gavin Daniel <gavindaniel@email.arizona.edu>
2022-08-14 15:04:56 -0700

    Initial Commit

---
+++ Mariners/MarinersApp.swift
@@ -0,0 +1,17 @@
+//
+//  MarinersApp.swift
+//  Mariners
+//
+//  Created by Gavin Daniel on 8/14/22.
+//
+
+import SwiftUI
+
+@main
+struct MarinersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
